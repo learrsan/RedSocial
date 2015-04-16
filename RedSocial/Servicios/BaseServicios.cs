f92716a945e3fbea2bf22c0d9fadd4f39828a046
learrsan <gaslas@hotmail.com>
2015-04-16 19:49:15 +0000

    16/04/2015 19:49

---
+++ RedSocial/Servicios/BaseServicios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedSocial.Servicios
+{
+    public class BaseServicios
+    {
+        public String Url { get; set; }
+
+        public BaseServicios(String url)
+        {
+            Url = url;
+        }
+    }
+}
---
+++ RedSocial/Utilidades/Serializador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace RedSocial.Utilidades
+{
+    public class Serializador
+    {
+        public static String Serializar<T>(T objeto)
+        {
+            var serializer = new JavaScriptSerializer();
+            var res = serializer.Serialize(objeto);
+            return res;
+        }
+
+        public static T Deserializar<T>(String texto)
+        {
+            var serializer = new JavaScriptSerializer();
+            var res = serializer.Deserialize<T>(texto);
+            return res;
+        }
+    }
+}
